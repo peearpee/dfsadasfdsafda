cee6b7840750e384c5f9446239f6f1a0489fc2a0
Patrick <eeypr@nottingham.ac.uk>
2017-12-04 09:17:19 +0000

    first commit

---
+++ adder.cpp
@@ -0,0 +1,9 @@
+//
+//  adder.cpp
+//  calc
+//
+//  Created by Patariki on 11/9/17.
+//  Copyright © 2017 Patariki. All rights reserved.
+//
+
+#include "adder.hpp"
---
+++ adder.hpp
@@ -0,0 +1,14 @@
+//
+//  adder.hpp
+//  calc
+//
+//  Created by Patariki on 11/9/17.
+//  Copyright © 2017 Patariki. All rights reserved.
+//
+
+#ifndef adder_hpp
+#define adder_hpp
+
+#include <stdio.h>
+
+#endif /* adder_hpp */
